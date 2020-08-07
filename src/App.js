46340ad4c00400579e0893d1fd85e87af1937c29
“aritramullick” <aritramullick@g.ucla.edu>
2020-08-07 18:53:20 +0400

    Next commits

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import besideHeading from './Assets/besideHeading.png'
 
 function App() {
   return (
     <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <div className="Heading">
+            <img src = {besideHeading} alt='headingLogo'/>
+              <div className="Name"> <span>Left, Right, Center! </span></div>
+            </div>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+            
           <a
             className="App-link"
             href="https://reactjs.org"
